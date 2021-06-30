9e0959c0c0b67def0ea3f02ebb49874f974721a4
rakhigit <raakhimeharwade09@gmail.com>
2021-06-30 17:52:24 +0530

    Updating

--- src/com/thirdparty/IsPrime.groovy
+++ src/com/thirdparty/IsPrime.groovy
@@ -1,13 +1,11 @@
 #!/usr/bin/env groovy
+
 package com.thirdparty
 @Grab('org.apache.commons:commons-math3:3.4.1')
 import org.apache.commons.math3.primes.Primes
 class IsPrime {
-   static void parallelize(int count) {
-  if (!Primes.isPrime(count)) {
-    error "${count} was not prime"
-  }
-  // …
-}
-   static String foo = "bar"
+    static boolean check(int count) {
+        return Primes.isPrime(count)
+    }
+    static String foo = "bar"
 }
